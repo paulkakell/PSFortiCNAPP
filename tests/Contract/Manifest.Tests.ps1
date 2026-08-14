019cd1d31e2d7094f61273ddf0bebc35ca0a7223
Paul K <59290939+paulkakell@users.noreply.github.com>
2026-08-13 19:13:36 -0600

    test: align manifest exports with Chapter 4

--- tests/Contract/Manifest.Tests.ps1
+++ tests/Contract/Manifest.Tests.ps1
@@ -3,9 +3,17 @@
 
 Describe 'PSFortiCNAPP module manifest contract' {
     BeforeAll {
-        $script:RepositoryRoot = (Resolve-Path -LiteralPath (Join-Path -Path $PSScriptRoot -ChildPath '../..')).Path
-        $script:ManifestPath = Join-Path -Path $script:RepositoryRoot -ChildPath 'src/PSFortiCNAPP/PSFortiCNAPP.psd1'
-        $script:Manifest = Test-ModuleManifest -Path $script:ManifestPath -ErrorAction Stop
+        $script:RepositoryRoot = (
+            Resolve-Path -LiteralPath (
+                Join-Path -Path $PSScriptRoot -ChildPath '../..'
+            )
+        ).Path
+        $script:ManifestPath = Join-Path `
+            -Path $script:RepositoryRoot `
+            -ChildPath 'src/PSFortiCNAPP/PSFortiCNAPP.psd1'
+        $script:Manifest = Test-ModuleManifest `
+            -Path $script:ManifestPath `
+            -ErrorAction Stop
     }
 
     It 'uses the initial semantic version' {
@@ -13,21 +21,33 @@ Describe 'PSFortiCNAPP module manifest contract' {
     }
 
     It 'requires PowerShell 7.6 and the Core edition' {
-        $script:Manifest.PowerShellVersion | Should -Be ([version]'7.6')
-        $script:Manifest.CompatiblePSEditions | Should -Contain 'Core'
+        $script:Manifest.PowerShellVersion |
+            Should -Be ([version]'7.6')
+        $script:Manifest.CompatiblePSEditions |
+            Should -Contain 'Core'
     }
 
-    It 'exports exactly the approved Chapter 3 functions' {
-        $functions = @($script:Manifest.ExportedFunctions.Keys | Sort-Object)
+    It 'exports exactly the approved Chapter 4 functions' {
+        $functions = @(
+            $script:Manifest.ExportedFunctions.Keys |
+                Sort-Object
+        )
 
-        $functions | Should -HaveCount 3
-        $functions | Should -Contain 'ConvertTo-FortiCNAPPEvidenceRecord'
-        $functions | Should -Contain 'Get-FortiCNAPPModuleInfo'
-        $functions | Should -Contain 'Test-FortiCNAPPEnvironment'
+        $functions | Should -HaveCount 4
+        $functions |
+            Should -Contain 'ConvertFrom-FortiCNAPPHttpExchange'
+        $functions |
+            Should -Contain 'ConvertTo-FortiCNAPPEvidenceRecord'
+        $functions |
+            Should -Contain 'Get-FortiCNAPPModuleInfo'
+        $functions |
+            Should -Contain 'Test-FortiCNAPPEnvironment'
     }
 
     It 'contains project and license metadata' {
-        $script:Manifest.PrivateData.PSData.ProjectUri | Should -Be 'https://github.com/paulkakell/PSFortiCNAPP'
-        $script:Manifest.PrivateData.PSData.LicenseUri | Should -Match '/LICENSE$'
+        $script:Manifest.PrivateData.PSData.ProjectUri |
+            Should -Be 'https://github.com/paulkakell/PSFortiCNAPP'
+        $script:Manifest.PrivateData.PSData.LicenseUri |
+            Should -Match '/LICENSE$'
     }
 }
